f22a2ea64727e955d676404fb3eaef3c993f5389
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-04-30 13:41:17 +0800

    First KV commmit

---
+++ overrides.js
@@ -0,0 +1,22 @@
+addEventListener('fetch', event => {
+    return event.respondWith(handleRequest(event.request));
+})
+
+async function handleRequest(request) {
+
+const newRequest = new URL(request.url)
+
+const { asset, regex, ...cache } = cacheAssets.find( ({regex}) => newRequest.pathname.match(regex)) ?? {}
+
+const newResponse = await fetch(request,
+        { cf:
+            {
+                resolveOverride: resolve.origin
+                    },
+            },
+        })
+
+const response = new Response(newResponse.body, newResponse)
+response.headers.set('debug', JSON.stringify(cache))
+return response
+}
