1c11b5bd2bce67b2f0a62734e51d9dc771e10c21
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-04 14:07:59 +0800

    new objs v3

--- overrides.js
+++ overrides.js
@@ -6,27 +6,20 @@ async function handleRequest(request) {
 
 const newRequest = new URL(request.url)
 
-
 const origin = [
     { 'new_host': 'home.erfianugrah.best', 'incoming_path': '/home', 'incoming_port': 1234 },
     { 'new_host': 'www.erfianugrah.net', 'incoming_path': '/net', 'incoming_port': 1234 },
     { 'new_host': 'www.erfi.ml', 'incoming_path': '/ml', 'incoming_port': 1234 },
     { 'new_host': 'www.google.com', 'incoming_path': '/google', 'incoming_port': 1234 },
     { 'new_host': 'http.erfianugrah.com', 'incoming_path': '/http', 'incoming_port': 1234 },
-    /*await override_variables.get("home"),
-    await override_variables.get("net"),
-    await override_variables.get("ml")*/
 ]
 
 const resolve = origin.find( ({ incoming_path }) => newRequest.pathname == incoming_path) ?? {}
 
-newRequest.hostname = resolve.new_host
-newRequest.pathname = ''
-
-
 const newResponse = await fetch(newRequest, 
     { cf:  { resolveOverride: resolve.new_host }} ) 
 
+newRequest.hostname = resolve.new_host
 
 const response = new Response(newResponse.body, newResponse)
 response.headers.set('debug-1', JSON.stringify(resolve))
