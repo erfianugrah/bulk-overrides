f308ef214ba93a92ea5c9f8d635f824c759de10b
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-04 12:23:39 +0800

    testing without CF obj

--- overrides.js
+++ overrides.js
@@ -7,9 +7,9 @@ async function handleRequest(request) {
 const newRequest = new URL(request.url)
 
 const origin = [
-    await override_variables.get("home"),  
-    await override_variables.get("net"),
-    await override_variables.get("ml")
+    override_variables.get("home"),  
+    override_variables.get("net"),
+    override_variables.get("ml")
 ]
 
 const resolve = origin.find( ({ incoming_path }) => newRequest.pathname.match(incoming_path)) ?? {}
@@ -17,10 +17,11 @@ const resolve = origin.find( ({ incoming_path }) => newRequest.pathname.match(in
 newRequest.host = resolve.new_host
 newRequest.pathname = ''
 
+/*
 const newResponse = await fetch(newRequest, 
-    { cf:  {resolveOverride: resolve.new_host }} ) 
-
-const response = new Response(newResponse.body, newResponse)
+    { cf:  { resolveOverride: resolve.new_host }} ) 
+*/
+const response = new Response(newRequest.body, newRequest)
 response.headers.set('debug-1', JSON.stringify(resolve))
 response.headers.set('debug-2', JSON.stringify(resolve.new_host))
 
