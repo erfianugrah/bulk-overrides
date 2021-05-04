160a3f8eac3f5ec7a5e51eda5e4dfcc3b7fdcce6
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-04 14:23:13 +0800

    new objs v6

--- overrides.js
+++ overrides.js
@@ -16,11 +16,11 @@ const origin = [
 
 const resolve = origin.find( ({ incoming_path }) => newRequest.pathname === incoming_path) ?? {}
 
-const newResponse = await fetch(newRequest, 
-    { cf:  { resolveOverride: resolve.new_host }} ) 
-
 newRequest.hostname = resolve.new_host
 newRequest.pathname = ''
+
+const newResponse = await fetch(newRequest, 
+    { cf:  { resolveOverride: resolve.new_host }} ) 
 
 const response = new Response(newResponse.body, newResponse)
 response.headers.set('debug-1', JSON.stringify(resolve))
