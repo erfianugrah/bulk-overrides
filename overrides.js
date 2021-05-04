a5e409b359b819bcf07cdc347ab4c2706459ff5e
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-04 12:29:25 +0800

    Test url request

--- overrides.js
+++ overrides.js
@@ -7,22 +7,21 @@ async function handleRequest(request) {
 const newRequest = new URL(request.url)
 
 const origin = [
-    override_variables.get("home"),  
-    override_variables.get("net"),
-    override_variables.get("ml")
+    await override_variables.get("home"),  
+    await override_variables.get("net"),
+    await override_variables.get("ml")
 ]
 
 const resolve = origin.find( ({ incoming_path }) => newRequest.pathname.match(incoming_path)) ?? {}
 
-const subRequest = new Request(request)
-subRequest.host = resolve.new_host
-subRequest.pathname = ''
+newRequest.host = resolve.new_host
+newRequest.pathname = ''
 
-/*
+
 const newResponse = await fetch(newRequest, 
     { cf:  { resolveOverride: resolve.new_host }} ) 
-*/
-const response = new Response(subRequest.body, subRequest)
+
+const response = new Response(newResponse.body, newResponse)
 response.headers.set('debug-1', JSON.stringify(resolve))
 response.headers.set('debug-2', JSON.stringify(resolve.new_host))
 
