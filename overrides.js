21931bd53eaa3f02e9b94582e888e95713726369
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-04 10:44:15 +0800

    fixed CF obj and KV

--- overrides.js
+++ overrides.js
@@ -7,26 +7,22 @@ async function handleRequest(request) {
 const newRequest = new URL(request.url)
 
 const origin = [
-    await override_variables.get("home"), // { 'new_host': 'home.erfianugrah.best', 'incoming_path': '/home', 'incoming_port': 1234 }
-    await override_variables.get("net"), // { 'new_host': 'www.erfianugrah.net', 'incoming_path': '/net', 'incoming_port': 1234 }
-    await override_variables.get("ml") // { 'new_host': 'www.erfi.ml', 'incoming_path': '/ml', 'incoming_port': 1234 }
+    await override_variables.get("home"),  
+    await override_variables.get("net"),
+    await override_variables.get("ml")
 ]
 
-const resolve = origin.find( ({ incoming_path }) => newRequest.pathname == incoming_path ) ?? {}
+const resolve = origin.find( ({ incoming_path }) => newRequest.pathname.match(incoming_path)) ?? {}
 
-//newRequest.port = resolve.incoming_port
 newRequest.host = resolve.new_host
-//newRequest.pathname = '/'
-/*
-const newResponse = await fetch(newRequest,
-        { cf:
-            {
-                resolveOverride: resolve.new_host
-                    },
-        },)
-*/
-const response = new Response(newRequest.body, newRequest)
+newRequest.pathname = ''
+
+let response = await fetch(newRequest, 
+    { cf:  {resolveOverride: resolve.new_host }} ) 
+
+const response = new Response(response.body, response)
 response.headers.set('debug-1', JSON.stringify(resolve))
 response.headers.set('debug-2', JSON.stringify(resolve.new_host))
+
 return response
 }
