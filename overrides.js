25acd7fa427cc6961986a106f6e9204a19577c3d
Erfi Anugrah <erfi.anugrah@gmail.com>
2021-05-04 09:03:41 +0800

    set json type

--- overrides.js
+++ overrides.js
@@ -7,9 +7,9 @@ async function handleRequest(request) {
 const newRequest = new URL(request.url)
 
 const origin = [
-    await override_variables.get("home"), // { 'new_host': 'home.erfianugrah.best', 'incoming_path': '/home', 'incoming_port': 1234 }
-    await override_variables.get("net"), // { 'new_host': 'www.erfianugrah.net', 'incoming_path': '/net', 'incoming_port': 1234 }
-    await override_variables.get("ml") // { 'new_host': 'www.erfi.ml', 'incoming_path': '/ml', 'incoming_port': 1234 }
+    await override_variables.get("home", {type: "json"}), // { 'new_host': 'home.erfianugrah.best', 'incoming_path': '/home', 'incoming_port': 1234 }
+    await override_variables.get("net", {type: "json"}), // { 'new_host': 'www.erfianugrah.net', 'incoming_path': '/net', 'incoming_port': 1234 }
+    await override_variables.get("ml", {type: "json"}) // { 'new_host': 'www.erfi.ml', 'incoming_path': '/ml', 'incoming_port': 1234 }
 ]
 
 const resolve = origin.find( ({incoming_path}) => newRequest.pathname === incoming_path ) ?? {}
